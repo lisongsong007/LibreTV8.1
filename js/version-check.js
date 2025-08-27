e1c6849537f31c7079821a06b2403dc2edf62589
lisongsong007 <1159872783@qq.com>
2025-08-28 00:37:10 +0800

    更新 version-check.js

--- js/version-check.js
+++ js/version-check.js
@@ -1,4 +1,4 @@
-<!--
+/*
 
 // 添加动画样式
 (function() {
@@ -188,4 +188,4 @@ function displayVersionElement(element) {
 // 页面加载完成后添加版本信息
 document.addEventListener('DOMContentLoaded', addVersionInfoToFooter);
 
--->
+*/
